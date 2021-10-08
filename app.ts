4165ac3faac7f29bd7ee69991831c789fb2e846c
NikDoe <nikd0e@yandex.ru>
2021-10-08 13:31:12 +0300

    feature(ts): arrays types

--- app.ts
+++ app.ts
@@ -4,7 +4,16 @@
 // } = {
 const person = {
     name: 'NikDoe',
-    age: 28
+    age: 28,
+    hobbies: ['Sports', 'Cooking']
 };
 
+let favoriteActivities: string[];
+favoriteActivities = ['Sports'];
+
 console.log(person.name);
+
+for (const hobby of person.hobbies) {
+    console.log(hobby.toUpperCase());
+    // console.log(hobby.map()); // !!! ERROR !!!
+}
