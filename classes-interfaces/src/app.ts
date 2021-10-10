46c8ef67a0954d51b021a6c100befe88163cf5d0
NikDoe <nikd0e@yandex.ru>
2021-10-10 09:17:08 +0300

    feature(ts): access modifier

--- classes-interfaces/src/app.ts
+++ classes-interfaces/src/app.ts
@@ -1,5 +1,6 @@
 class Department {
-    name: string;
+    public name: string;
+    private employees: string[] = [];
 
     constructor(n: string) {
         this.name = n;
@@ -9,14 +10,32 @@ class Department {
     describe(this: Department) {
         console.log('Department: ' + this.name);
     }
+
+    addEmployee(employee: string) {
+        this.employees.push(employee);
+    }
+
+    printEmployeeInformation() {
+        console.log(this.employees.length);
+        console.log(this.employees);
+    }
 }
-
-const accounting = new Department('Accounting');
-
-accounting.describe();
 
 //так как accountingCopy не является инстансом Department, то мы не сможем вызвать у него describe();
 //чтобы мы могли вызывать методы, нам нужно добавить поле name, так ts будет видеть что обьект похожь на Department
+/*
 const accountingCopy = { name: 'DUMMY', describe: accounting.describe };
 
 accountingCopy.describe();
+*/
+
+const accounting = new Department('Accounting');
+
+accounting.addEmployee('Nik');
+accounting.addEmployee('Vik');
+accounting.addEmployee('Alen');
+
+
+// accounting.employees[2] = 'Alen'; // Error, private
+accounting.describe();
+accounting.printEmployeeInformation();
