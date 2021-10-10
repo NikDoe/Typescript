0efadfbd9d63132632cd9ff9d3863c0ea25672b3
NikDoe <nikd0e@yandex.ru>
2021-10-10 19:59:45 +0300

    init (advanced): guard of the unions types

--- advanced/src/app.ts
+++ advanced/src/app.ts
@@ -13,7 +13,7 @@ type Employee = {
 type ElevatedEmployee = Admin & Employee; //analogy of interface ElevatedEmployee
 
 const e1: ElevatedEmployee = {
-    name: 'Max',
+    name: 'NikDoe',
     privileges: ['create-server'],
     startDate: new Date()
 };
@@ -22,3 +22,27 @@ type Combinable = string | number;
 type Numeric = number | boolean;
 
 type Universal = Combinable & Numeric;
+
+function add(a: Combinable, b: Combinable) {
+    if (typeof a === 'string' || typeof b === 'string') {
+        return a.toString() + b.toString();
+    }
+    return a + b;
+}
+
+console.log(add('1', 2))
+
+type UnknownEmployee = Employee | Admin;
+
+function printEmployeeInformation(employee: UnknownEmployee) {
+    console.log('Name: ' + employee.name)
+    if ('privileges' in employee) {
+        console.log('Privileges: ' + employee.privileges);
+    }
+    if ('startDate' in employee) {
+        console.log('Start Date: ' + employee.startDate);
+    }
+}
+
+printEmployeeInformation(e1)
+printEmployeeInformation({name: 'Vik', startDate: new Date()})
