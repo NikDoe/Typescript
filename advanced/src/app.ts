399a08896de1405917b39f5ac008c5ad826b0dad
NikDoe <nikd0e@yandex.ru>
2021-10-10 19:38:47 +0300

    init (advanced): intersection types

--- advanced/src/app.ts
+++ advanced/src/app.ts
@@ -1,1 +1,24 @@
-console.log('Advanced types');
+type Admin = {
+    name: string;
+    privileges: string[];
+};
+
+type Employee = {
+    name: string;
+    startDate: Date;
+};
+
+// interface ElevatedEmployee extends Employee, Admin {}
+
+type ElevatedEmployee = Admin & Employee; //analogy of interface ElevatedEmployee
+
+const e1: ElevatedEmployee = {
+    name: 'Max',
+    privileges: ['create-server'],
+    startDate: new Date()
+};
+
+type Combinable = string | number;
+type Numeric = number | boolean;
+
+type Universal = Combinable & Numeric;
