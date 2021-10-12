a01e2cb624d2f0e81ff842d2d83381bc015a03b9
NikDoe <nikd0e@yandex.ru>
2021-10-12 08:24:51 +0300

    feature (advanced): guard of classes

--- advanced/src/app.ts
+++ advanced/src/app.ts
@@ -46,3 +46,34 @@ function printEmployeeInformation(employee: UnknownEmployee) {
 
 printEmployeeInformation(e1)
 printEmployeeInformation({name: 'Vik', startDate: new Date()})
+
+class Car {
+    drive() {
+        console.log('Driving...');
+    }
+}
+
+class Truck {
+    drive() {
+        console.log('Driving a truck...');
+    }
+
+    loadCargo(amount: number) {
+        console.log('Loading cargo ...' + amount);
+    }
+}
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVehicle(vehicle: Vehicle) {
+    vehicle.drive();
+    if (vehicle instanceof Truck) {
+        vehicle.loadCargo(1000);
+    }
+}
+
+useVehicle(v1);
+useVehicle(v2);
