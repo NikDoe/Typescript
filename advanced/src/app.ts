9210b6fbfb3ebcfbbe09b4e02278c4d5242bdf4c
NikDoe <nikd0e@yandex.ru>
2021-10-12 09:04:15 +0300

    feature (advanced): type casting

--- advanced/src/app.ts
+++ advanced/src/app.ts
@@ -109,3 +109,16 @@ function moveAnimal(animal: Animal) {
 }
 
 moveAnimal({type: 'bird', flyingSpeed: 10});
+
+//TYPE CASTING
+//IF WE KNOW THAT ELEMENT NOT NULL..we use "!"
+// const userInputElement = <HTMLInputElement>document.getElementById('user-input')!;
+// const userInputElement = document.getElementById('user-input')! as HTMLInputElement;
+// userInputElement.value = 'Hi there!'
+
+
+//IF WE ARE NOT SURE OF NULL...we must use if
+const userInputElement = document.getElementById('user-input')
+if (userInputElement) {
+    (userInputElement as HTMLInputElement).value = 'Hi there!';
+}
