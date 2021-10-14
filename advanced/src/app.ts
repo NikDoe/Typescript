b3fd9cc76e8283a6c2dcda072dc9cb8df6465679
NikDoe <nikd0e@yandex.ru>
2021-10-14 10:44:39 +0300

    feature (advanced): index properties

--- advanced/src/app.ts
+++ advanced/src/app.ts
@@ -122,3 +122,13 @@ const userInputElement = document.getElementById('user-input')
 if (userInputElement) {
     (userInputElement as HTMLInputElement).value = 'Hi there!';
 }
+
+//INDEX PROPERTIES
+interface ErrorContainer { // { email: 'Not a valid email', username: 'Must start with a character!' }
+    [prop: string]: string;
+}
+
+const errorBag: ErrorContainer = {
+    email: 'Not a valid email!',
+    username: 'Must start with a capital character!'
+};
