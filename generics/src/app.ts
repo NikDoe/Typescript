5e804a18fb65a32847fff05f739ada13c877f861
NikDoe <nikd0e@yandex.ru>
2021-10-14 15:38:32 +0300

    feature(generics):work w/ constraints

--- generics/src/app.ts
+++ generics/src/app.ts
@@ -21,3 +21,12 @@ const merge = <T, U> (objA: T, objB?: U) => {
 const mergeObj = merge({name: 'Nik'}, {age: 28})
 const newMergeObj = merge({isLogged: true})
 console.log(mergeObj.name, newMergeObj.isLogged)
+
+
+//CONSTRAINTS(ограничения)
+function newMerge<T extends object, U extends object>(objA: T, objB: U) {
+    return Object.assign(objA, objB);
+}
+
+const mergedObj = newMerge({ name: "Max", hobbies: ["Sports"] }, { age: 30 });
+console.log(mergedObj);
