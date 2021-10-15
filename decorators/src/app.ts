1e8eed1db9b1aafa035ff38c563abe384524a87c
NikDoe <nikd0e@yandex.ru>
2021-10-15 10:26:27 +0300

    feature(decorators): property decorator

--- decorators/src/app.ts
+++ decorators/src/app.ts
@@ -30,4 +30,31 @@ class Person {
 
 const newPerson = new Person();
 
-console.log(newPerson);
+//DIFFERENT DECORATORS
+function Log (target: any, propName: string | symbol) {
+    console.log('Property Decorator!')
+    console.log(target, propName);
+}
+
+class Product {
+    @Log
+    public title: string
+    private _price: number
+
+    set price(val: number) {
+        if (val > 0) {
+            this._price = val;
+        } else {
+            throw new Error('Invalid price - should be positive!');
+        }
+    }
+
+    constructor(t: string, p: number) {
+        this.title = t;
+        this._price = p;
+    }
+
+    getPriceWithTax(tax: number) {
+        return this._price * (1 + tax);
+    }
+}
