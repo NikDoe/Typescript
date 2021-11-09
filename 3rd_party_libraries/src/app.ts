b859abf6967c113fecf6eb6d71bdfd148bea106e
NikDoe <nikd0e@yandex.ru>
2021-11-09 14:42:57 +0300

    feature(3rd_p_lib): class-transformer

--- 3rd_party_libraries/src/app.ts
+++ 3rd_party_libraries/src/app.ts
@@ -1,4 +1,17 @@
 import { Product } from './product.model';
+import 'reflect-metadata';
+import { plainToClass } from 'class-transformer';
 
-const p1 = new Product('Book', 12.99);
-console.log(p1);
+const products = [
+    {title: "A book", price: 12.99},
+    {title: "A carpet", price: 24.99}
+]
+// const loadedProducts = products.map(prod => {
+//     return new Product(prod.title, prod.price)
+// })
+
+const loadedProducts = plainToClass(Product, products);
+
+for (const prod of loadedProducts) {
+    console.log(prod.getInformation());
+}
