58a6e760bae704060d1585bf4614938bc3342136
NikDoe <nikd0e@yandex.ru>
2021-11-12 17:29:50 +0300

    feature(gmaps): fetch coords

--- google_maps/src/app.ts
+++ google_maps/src/app.ts
@@ -1,10 +1,31 @@
+import axios from "axios";
+
 const form = document.querySelector('form')!;
 const addressInput = document.getElementById('address')! as HTMLInputElement;
+
+const GOOGLE_API_KEY = 'AIzaSyBMX-lPckqHEP1VCE2CWyhDRk48QugHLDc';
+
+type GoogleGeocodingResponse = {
+    results: {geometry: {location: {lat: number, lng: number}}}[];
+    status: "OK" | "ZERO_RESULTS";
+};
 
 function searchAddressHandler (event: Event) {
     event.preventDefault();
     const enteredAddress = addressInput.value;
 
-    console.log(enteredAddress);
+    axios.get<GoogleGeocodingResponse>(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(enteredAddress)}&key=${GOOGLE_API_KEY}`)
+        .then(
+            response => {
+                if(response.data.status !== "OK"){
+                    throw new Error('Could not fetch location ❌');
+                }
+                const coords = response.data.results[0].geometry.location;
+                console.log(coords);
+            }
+        )
+        .catch(
+            error => console.log(error)
+        );
 }
 form.addEventListener('submit', searchAddressHandler);
